046f7ff35afe2904bacc4356935ac1e9d01cb2f6
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-08-10 03:27:33 -0700

    finished demo follow-along

--- api/aloha.py
+++ api/aloha.py
@@ -10,9 +10,13 @@ class handler(BaseHTTPRequestHandler):
     self.end_headers()
 
     url_components = parse.urlsplit(self.path)
-    
+    query_string_list = parse.parse_qsl(url_components.query)
 
-    message = self.path
-    # message = f"Aloha {friend}"
+    query_dict = dict(query_string_list)
+
+    friend = query_dict["friend"]
+
+    # message = str(query_dict)
+    message = f"Aloha {friend}"
     self.wfile.write(message.encode())
     return
