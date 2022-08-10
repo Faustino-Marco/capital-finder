964a4fa7e768e359069af91fc7a2206c694eeddd
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-08-10 04:33:41 -0700

    input: city name, find country name

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -16,23 +16,39 @@ class handler(BaseHTTPRequestHandler):
     query_string_list = parse.parse_qsl(url_components.query)
 
     query_dict = dict(query_string_list)
+    print("query_dict: ", query_dict)
 
     if "country" in query_dict:
       url = "https://restcountries.com/v3.1/name/"
       response = requests.get(url + query_dict["country"])
       data = response.json()
-      cap_cities = []
+      cap_cities = [] 
 
       for country_data in data:
-        cap_name = country_data["capital"][0]
+        cap_name = country_data["capital"]
+        print(cap_name)
 
         cap_cities.append(cap_name)
 
       string_cap = str(cap_cities)
       country = query_dict["country"]
-      message = f"The capital of {country} is {cap_cities[0]}"
-    else:
-      message = "name a country"
+      message = f"The capital of {country} is {cap_cities[0][0]}."
+
+    if "capital" in query_dict:
+      url = "https://restcountries.com/v3.1/capital/"
+      response = requests.get(url + query_dict["capital"])
+      data = response.json()
+      home_country = []
+
+      for city_data in data:
+        country_name = city_data["name"]["official"]
+
+        home_country.append(country_name)
+
+      string_country = str(home_country)
+      city = query_dict["capital"]
+      message = f"{city} is the capital of {country_name}."
+    
 
     self.wfile.write(message.encode())
     return
